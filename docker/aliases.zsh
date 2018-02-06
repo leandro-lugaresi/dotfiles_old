7e341cd9c9150804dd3322c158742d58b4b8e146
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2018-02-06 17:49:56 -0200

    sync

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,5 +1,6 @@
 #!/bin/sh
 alias d='docker'
+alias compose='docker-compose'
 
 docker_prune() {
 	docker system prune --volumes -fa
--- fonts/install.sh
+++ fonts/install.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/.local/share/fonts/
 curl -L -s -o /tmp/anon.zip "http://www.marksimonson.com/assets/content/fonts/AnonymousPro-1.002.zip"
 unzip /tmp/anon.zip -d /tmp
-cp /tmp/AnonymousPro-1.002.001/*.ttf "~/.local/share/fonts/"
+cp /tmp/AnonymousPro-1.002.001/*.ttf ~/.local/share/fonts
 
 for type in Bold Light Medium Regular Retina; do
     wget -O ~/.local/share/fonts/FiraCode-${type}.ttf \
