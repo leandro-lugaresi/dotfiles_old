e3408c02d1bc67e27e77cc7facadcb87acdcf634
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2018-06-28 10:38:23 -0300

    fix problem with updates

--- fonts/install.sh
+++ fonts/install.sh
@@ -2,10 +2,11 @@
 
 mkdir -p ~/.local/share/fonts/
 curl -L -s -o /tmp/anon.zip "http://www.marksimonson.com/assets/content/fonts/AnonymousPro-1.002.zip"
-unzip /tmp/anon.zip -d /tmp
-cp /tmp/AnonymousPro-1.002.001/*.ttf ~/.local/share/fonts
+unzip -u /tmp/anon.zip -d /tmp
+cp -n /tmp/AnonymousPro-1.002.001/*.ttf ~/.local/share/fonts
 
 for type in Bold Light Medium Regular Retina; do
-    wget -O ~/.local/share/fonts/FiraCode-${type}.ttf \
+    wget -q -nc -O ~/.local/share/fonts/FiraCode-${type}.ttf \
     "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
 done
+rm -rf /tmp/AnonymousPro-1.002.001
